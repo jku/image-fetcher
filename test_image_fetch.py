fa3ca5686fa1eb241b77bb60962b8b9fd456d4c9
Jussi Kukkonen <jku@goto.fi>
2017-01-11 23:28:11 +0200

    Add test for find_image_urls()

--- test_image_fetch.py
+++ test_image_fetch.py
@@ -1,17 +1,37 @@
 import image_fetch
 from mock import patch, Mock
 
+# Tests compatible with pytest-3
+
+# Create a mock of urllib.urlopen() return value: it's an object
+# with a read() function that returns the html.
+html = """<!DOCTYPE html>
+          <html>
+          <body>
+          <img src="cat.gif">
+          <img src="dir/relative_cat.gif">
+          <img src="http://test.com/absolute_cat.gif">
+          </body>
+          </html>"""
+attrs = {'read.return_value':html}
+urlopen_mock_obj = Mock(**attrs)
+
+
 @patch('image_fetch.urlopen')
 def test_find_image_src_attributes(mock_urlopen):
-    a = Mock()
-    a.read.side_effect = ["""<!DOCTYPE html>
-              <html><body>
-              <img src="cat.gif">
-              <img src="dir/relative_cat.gif">
-              <img src="http://test.com/absolute_cat.gif">
-              </body></html>"""]
-    mock_urlopen.return_value = a
+    mock_urlopen.return_value = urlopen_mock_obj
 
-    expected = ['cat.gif', 'dir/relative_cat.gif', 'http://test.com/absolute_cat.gif']
+    expected = ['cat.gif',
+                'dir/relative_cat.gif',
+                'http://test.com/absolute_cat.gif']
     assert image_fetch.find_image_src_attributes("http://example.com") == expected
+
+@patch('image_fetch.urlopen')
+def test_find_image_urls(mock_urlopen):
+    mock_urlopen.return_value = urlopen_mock_obj
+
+    expected = ['http://example.com/cat.gif',
+                'http://example.com/dir/relative_cat.gif',
+                'http://test.com/absolute_cat.gif']
+    assert image_fetch.find_image_urls("http://example.com") == expected
 
