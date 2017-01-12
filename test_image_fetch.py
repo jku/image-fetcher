1af612be3968609b6f240643c53feff53e7919df
Jussi Kukkonen <jku@goto.fi>
2017-01-12 20:47:54 +0200

    Add test for file name generation

--- test_image_fetch.py
+++ test_image_fetch.py
@@ -1,4 +1,5 @@
 import image_fetch
+import os, tempfile
 from mock import patch, Mock
 
 # Tests compatible with pytest-3
@@ -25,4 +26,17 @@ def test_find_image_urls(mock_urlopen):
                 'http://example.com/dir/relative_cat.gif',
                 'http://test.com/absolute_cat.gif']
     assert image_fetch._find_image_urls("http://example.com") == expected
+
+def test_get_filename_for_url():
+    tmpdir = tempfile.mkdtemp()
+    handle, tmpfile = tempfile.mkstemp(dir=tmpdir)
+    fn1 = image_fetch._get_filename_for_url("http://example.com/dir/file.ext", tmpdir)
+    fn2 = image_fetch._get_filename_for_url("http://example.com/" + os.path.basename(tmpfile), tmpdir)
+    fn3 = image_fetch._get_filename_for_url("http://example.com/dir/", tmpdir)
+    os.remove(tmpfile)
+    os.rmdir(tmpdir)
+
+    assert fn1 == tmpdir + "/file.ext"
+    assert fn2 == tmpfile + "(2)"
+    assert fn3 == tmpdir + "/unnamed"
 
